5ba1534351bd06ed66c6a74b9df752b6534c6674
SyuzannaAyvazyan <153364612+SyuzannaAyvazyan@users.noreply.github.com>
2024-04-18 20:19:37 +0400

    BooksController.cs

--- BooksController.cs
+++ BooksController.cs
@@ -1,36 +1,51 @@
-﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Technolab.OnlineLibrary.Web.Models;
 using Technolab.OnlineLibrary.Web.ViewModels;
+ 
+ 
+using System; 
+using System.Linq; 
+using Microsoft.AspNetCore.Authorization; 
+using Microsoft.AspNetCore.Mvc; 
+using Technolab.OnlineLibrary.Web.Models; 
+using Technolab.OnlineLibrary.Web.ViewModels; 
+ 
+namespace Technolab.OnlineLibrary.Web.Controllers 
+{ 
+    [AllowAnonymous] 
+    public class BooksController : Controller 
+    { 
+        private readonly ILibraryDbContextFactory _contextFactory; 
+ 
+        public BooksController(ILibraryDbContextFactory contextFactory) 
+        { 
+            _contextFactory = contextFactory; 
+        } 
+ 
+        public IActionResult Index(string searchTerm) 
+        { 
+            using var context = _contextFactory.Create();
 
-namespace Technolab.OnlineLibrary.Web.Controllers
-{
-    [AllowAnonymous]
-    public class BooksController : Controller
-    {
-        public BooksController(ILibraryDbContextFactory contextFactory)
-        {
-            this.ContextFactory = contextFactory;
-        }
+            List<Book> books = context.Books;
 
-        public IActionResult Index()
-        {
-
-            using var context = ContextFactory.Create();
+            if (!string.IsNullOrWhiteSpace(searchTerm))
+            {
+                books = books.Where(x => x.Title.Contains(searchTerm) || x.Author.Contains(searchTerm)).ToList();
+            }
 
             var model = new BookSearchViewModel
             {
-                Books = context.Books.Take(10).Select(x => new BookViewModel
+                Books = books.Take(10).Select(x => new BookViewModel
                 {
                     Id = x.Id,
                     Title = x.Title,
                     Author = x.Author
-                }).ToList()
+                }).ToList(),
+                SearchTerm = searchTerm
             };
-            
-            return View(model);
-        }
 
-        public ILibraryDbContextFactory ContextFactory { get; }
-    }
+            return View(model); 
+        } 
+    } 
 }
