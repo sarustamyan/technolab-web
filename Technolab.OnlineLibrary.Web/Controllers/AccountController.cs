b88982958d181ccf3c10ef24cc487250ff92a322
Gor Nikoghosyan <gor.mainaddress@gmail.com>
2024-09-14 23:51:14 +0400

    Show Change Password page to user

--- Technolab.OnlineLibrary.Web/Controllers/AccountController.cs
+++ Technolab.OnlineLibrary.Web/Controllers/AccountController.cs
@@ -10,5 +10,10 @@ namespace Technolab.OnlineLibrary.Web.Controllers
         {
             return View();
         }
+
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
     }
 }
---
+++ Technolab.OnlineLibrary.Web/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,8 @@
+namespace Technolab.OnlineLibrary.Web.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        public string NewPassword { get; set; }
+        public string ConfirmNewPassword { get; set; }
+    }
+}
